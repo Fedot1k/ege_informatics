71c4e56e37c6842cc377322497f79b8e14dad60d
Fedot1k <frasssepkin@gmail.com>
2024-10-21 20:59:03 +0300

    high fly kobe

--- 24-file.py
+++
@@ -1,9 +0,0 @@
-s = open().readline()
-
-res = []
-
-for i in range(len(s) - 2):
-    if s[i + 1] == s[i + 2]:
-        res.append(s[i])
-
-print(res.count('S'))
--- 25-fnmatch.py
+++
@@ -1,5 +0,0 @@
-from fnmatch import *
-
-for i in range(1756, 10**8, 1756):
-    if fnmatch(str(i), '12*45*3'):
-        print(i, i // 1756)
